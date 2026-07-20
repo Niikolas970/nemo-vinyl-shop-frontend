74b70922be77f653c7b35ddbc30643421c8c52a1
NicolasPirajan <nicolasalejandro2001@gmail.com>
2026-07-20 16:58:33 -0500

    feat: se activa la acciones de page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/nemo-vinyl-shop-frontend/',
   plugins: [react()],
   resolve: {
     alias: {
